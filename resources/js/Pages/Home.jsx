53d1032ce5048986720fc8f00921ff9d6eb7300e
ActualLearner <abdellahshafi7@gmail.com>
2025-03-16 16:30:39 +0300

    feat: preserve scroll

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,6 +1,11 @@
+import { Link } from "@inertiajs/react";
+
 function Home({ name }) {
   return (
+    <>
     <h1 className="title">Hello {name}</h1>
+    <Link preserveScroll className="block title mt-[1000px]" href="/">{new Date().toLocaleTimeString()}</Link>
+    </>
   )
 }
 
