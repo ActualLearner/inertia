72b74d10e0249882eb95133128e141ddd46338e4
ActualLearner <abdellahshafi7@gmail.com>
2025-03-17 07:45:55 +0300

    feat: add post model & resource controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
-Route::get('/', function () {
-    sleep(5);
-    return Inertia::render('Home', ['name' => "Mike"]);
-});
+Route::get('/', [PostController::class, 'index']);
+Route::resource('posts', PostController::class)->except('index');
